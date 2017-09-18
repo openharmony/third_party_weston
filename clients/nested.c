bloomberg/chromium.bb

commit ce5b614c80b4dfe8e8997602c601dc81b0c7c3e5

clients/nested: use weston_check_egl_extension over strstr
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>

clients/nested.c
@@ -748,7 +748,7 @@ nested_init_compositor(struct nested *nested)
 
 	nested->egl_display = display_get_egl_display(nested->display);
 	extensions = eglQueryString(nested->egl_display, EGL_EXTENSIONS);
-	if (strstr(extensions, "EGL_WL_bind_wayland_display") == NULL) {
+	if (weston_check_egl_extension(extensions, "EGL_WL_bind_wayland_display") == NULL) {
 		fprintf(stderr, "no EGL_WL_bind_wayland_display extension\n");
 		return -1;
 	}
@@ -771,7 +771,7 @@ nested_init_compositor(struct nested *nested)
 		const char *func = "eglCreateWaylandBufferFromImageWL";
 		const char *ext = "EGL_WL_create_wayland_buffer_from_image";
 
-		if (strstr(extensions, ext)) {
+		if (weston_check_egl_extension(extensions, ext)) {
 			create_wayland_buffer_from_image =
 				(void *) eglGetProcAddress(func);
 			use_ss_renderer = 1;

openharmony/third_party_weston

commit 107b363c500fffb052b6afda90a043ebff0b898d

clients/nested: fix boolean test
weston_check_egl_extension() returns a bool, not a pointer.

Fixes: ce5b614c80b4dfe8e899 "clients/nested: use weston_check_egl_extension
                             over strstr"
Cc: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

clients/nested.c
@@ -748,7 +748,7 @@ nested_init_compositor(struct nested *nested)
 
 	nested->egl_display = display_get_egl_display(nested->display);
 	extensions = eglQueryString(nested->egl_display, EGL_EXTENSIONS);
-	if (weston_check_egl_extension(extensions, "EGL_WL_bind_wayland_display") == NULL) {
+	if (!weston_check_egl_extension(extensions, "EGL_WL_bind_wayland_display")) {
 		fprintf(stderr, "no EGL_WL_bind_wayland_display extension\n");
 		return -1;
 	}